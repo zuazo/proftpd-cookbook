7e7a85997ccffc234c448db17c02525881ba9f9c
Xabier de Zuazo <xabier@onddo.com>
2014-09-28 11:31:58 +0200

    version bumped to 1.1.0 WiP

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
 description      'Installs and Configures ProFTPD ftp server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.1.0' # WiP
 
 depends 'ohai'
 depends 'yum-epel'
